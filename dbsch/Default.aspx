0feb4fa15bfa47eac2ad988517a360ca5862e2e2
leaflu0315 <johnson7511333@gmail.com>
2018-04-12 13:05:40 +0800

    search database by using asp.net

---
+++ dbsch/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 133px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Enter ID:"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" Width="213px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        <p>
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ dbsch/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        // connection string
+        string DBName = "DB405530038" , Server = "140.123.174.54", Username = "DB405530038", Password = "johnson7511333@yahoo.com.tw";
+        string TableName = "Info", FieldName = "ID";
+        string strConn = "Data Source = " + Server +
+                        "; Initial Catalog = " + DBName +
+                        "; User ID = "+ Username +
+                        "; Password = "+ Password +";";
+        string strSQL = " ";
+        //建立連接
+        SqlConnection myConn = new SqlConnection(strConn);
+        //打開連接
+        myConn.Open();
+        if (TextBox1.Text != "") strSQL = @"SELECT * FROM " + TableName + " WHERE [" + FieldName + "] = '" + TextBox1.Text + "'";
+        //set SQL command string
+        // prepare connection and SQL statement
+        SqlCommand myCommand = new SqlCommand(strSQL, myConn);
+        SqlDataReader myDataReader = myCommand.ExecuteReader();
+        Label2.Text = "";
+        while (myDataReader.Read())
+            if (myDataReader[FieldName].ToString() != ""){
+                String text = String.Format("學號: {0}, 姓名: {1}, 系所代碼: {2}", myDataReader["ID"].ToString(), myDataReader["Name"].ToString(), myDataReader["Department"].ToString());
+                Label2.Text += text;
+                Label2.Text += "<br/>";          
+            }
+        if (Label2.Text == "") Label2.Text = "No Result ! <br/>";
+        if (TextBox1.Text == "") Label2.Text = "Please enter the ID !<br/>";
+    }
+}
