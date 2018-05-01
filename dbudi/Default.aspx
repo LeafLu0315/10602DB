0d6b00a9e1438cb9c4f1ec7dec261ee9820a8845
leaflu0315 <johnson7511333@gmail.com>
2018-05-02 00:06:17 +0800

    database update delete insert

---
+++ dbudi/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Please click the action :<br />
+        <asp:Button ID="Button1" runat="server" Height="24px" OnClick="Button1_Click" Text="Insert" Width="61px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Height="24px" Text="Delete" Width="61px" OnClick="Button2_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Height="24px" Text="Update" Width="61px" OnClick="Button3_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="TableName:" Visible="False"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" Height="16px" Width="144px" Visible="False"></asp:TextBox>
+    
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Condition : " Visible="False"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server" Visible="False"></asp:TextBox>
+        <asp:Label ID="Label6" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Condition : " Visible="False"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" Visible="False"></asp:TextBox>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="submit" />
+    
+    </div>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Result : "></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ dbudi/Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+    /* Insert */
+    protected void Button1_Click(object sender, EventArgs e) {
+        Label4.Text = "Values :( ";
+        Label6.Text = ")";
+        Label1.Visible = true;
+        Label4.Visible = true;
+        Label5.Visible = false;
+        Label6.Visible = true;
+        TextBox1.Visible = true;
+        TextBox2.Visible = true;
+        TextBox3.Visible = false;
+        Global.act = 1;
+    }
+    /* Delete */
+    protected void Button2_Click(object sender, EventArgs e) {
+        Label4.Text = "Condition : ";
+        Label1.Visible = true;
+        Label4.Visible = true;
+        Label5.Visible = false;
+        Label6.Visible = false;
+        TextBox1.Visible = true;
+        TextBox2.Visible = true;
+        TextBox3.Visible = false;
+        Global.act = 2;
+    }
+    /* Update */
+    protected void Button3_Click(object sender, EventArgs e) {
+        Label4.Text = "SET : ";
+        Label1.Visible = true;
+        Label4.Visible = true;
+        Label5.Visible = true;
+        Label6.Visible = false;
+        TextBox1.Visible = true;
+        TextBox2.Visible = true;
+        TextBox3.Visible = true;
+        Global.act = 3;
+        
+    }
+    /* Submit */
+    protected void Button4_Click(object sender, EventArgs e) {
+        switch (Global.act) {
+            case 1:
+                Global.str = "INSERT INTO "+ TextBox1.Text+" VALUES(" + TextBox2.Text + ")";
+                break;
+            case 2:
+                Global.str = "DELETE FROM " + TextBox1.Text;
+                if (TextBox2.Text != "") Global.str += " WHERE " + TextBox2.Text;
+                break;
+            case 3:
+                Global.str = "UPDATE " + TextBox1.Text + " SET " + TextBox2.Text;
+                if (TextBox3.Text != "") Global.str += " WHERE " + TextBox3.Text;
+                break;
+            default:
+                break;
+        }
+        if (Global.str != "") {
+            Label3.Text = Global.str;
+            connectSQL(Global.str);
+        }
+        else Label3.Text = "NO action can be done <br/>";
+    }
+
+    public void connectSQL(string str){
+        // connection string
+        string DBName = "DB405530038", Server = "140.123.174.54", Username = "DB405530038", Password = "johnson7511333@yahoo.com.tw";
+        string TableName = TextBox1.Text;
+        string strConn = "Data Source = " + Server +
+                        "; Initial Catalog = " + DBName +
+                        "; User ID = " + Username +
+                        "; Password = " + Password + ";";
+        /* 建立連接 */
+        SqlConnection myConn = new SqlConnection(strConn);
+        /* 打開連接 */
+        myConn.Open();
+        // prepare connection and SQL statement
+        SqlCommand myCommand = new SqlCommand(str, myConn);
+        SqlDataReader myDataReader = myCommand.ExecuteReader();
+    }
+
+    
+}
+
+public class Global {
+    public static string str = "";
+    public static int act = 0;
+}
